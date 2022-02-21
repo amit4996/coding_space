21c30b229d1358c9b37d987fe951feb5d9eca215
amit4996 <69569478+amit4996@users.noreply.github.com>
2022-02-21 10:13:43 +0530

    Time: 34 ms (52.32%), Space: 13.3 MB (99.36%) - LeetHub

--- 785-is-graph-bipartite/785-is-graph-bipartite.cpp
+++ 785-is-graph-bipartite/785-is-graph-bipartite.cpp
@@ -1,24 +1,41 @@
 class Solution {
 public:
-    bool isbfsbipartite(int i,vector<vector<int>>& graph,vector<int> &color)
+    // bool isbfsbipartite(int i,vector<vector<int>>& graph,vector<int> &color)
+    // {
+    //         queue<int> q;
+    //         q.push(i);
+    //         color[i]=1;
+    //         while(!q.empty())
+    //         {
+    //             int node=q.front();
+    //             q.pop();
+    //             for(auto it:graph[node])
+    //             {
+    //                 if(color[it]==-1)
+    //                 {
+    //                     q.push(it);
+    //                     color[it]=1-color[node];
+    //                 }
+    //                 else if(color[it]==color[node]) return false;     
+    //             }
+    //         }
+    //     return true;
+    // }
+    bool isdfspartite(int i,vector<vector<int>> &graph,vector<int> & color)
     {
-            queue<int> q;
-            q.push(i);
-            color[i]=1;
-            while(!q.empty())
+        if(color[i]==-1) color[i]=1;
+        for(auto it:graph[i])
+        {
+            if(color[it]==-1)
             {
-                int node=q.front();
-                q.pop();
-                for(auto it:graph[node])
-                {
-                    if(color[it]==-1)
-                    {
-                        q.push(it);
-                        color[it]=1-color[node];
-                    }
-                    else if(color[it]==color[node]) return false;     
-                }
+                color[it]=1-color[i];
+               if(!isdfspartite(it,graph,color)) return false;
             }
+            else
+            {
+                if(color[it]==color[i]) return false;
+            }
+        }
         return true;
     }
     bool isBipartite(vector<vector<int>>& graph) {
@@ -28,7 +45,7 @@ public:
         {
             if(color[i]==-1)
             {
-                if(isbfsbipartite(i,graph,color)==false) return false;
+                if(isdfspartite(i,graph,color)==false) return false;
             }
         }
         return true;
