8f2338fc1eb8a9e32e427eeaab5e75b7383c9363
amit4996 <69569478+amit4996@users.noreply.github.com>
2022-04-16 23:14:22 +0530

    Added solution - LeetHub

--- Selection Sort - GFG/selection-sort.cpp
+++ Selection Sort - GFG/selection-sort.cpp
@@ -20,10 +20,10 @@ class Solution
      
     void selectionSort(int arr[], int n)
     {
-      for(int i=0;i<n;i++)
+      for(int i=0;i<=n-2;i++)
       {
           int min=i;
-          for(int j=i;j<n;j++)
+          for(int j=i+1;j<=n-1;j++)
           {
               if(arr[j]<arr[min])
               {
