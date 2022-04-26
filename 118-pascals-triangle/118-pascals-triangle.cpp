601f126886f75c31f9ac5003a69d76104c0a7302
amit4996 <69569478+amit4996@users.noreply.github.com>
2022-04-26 15:53:07 +0530

    Time: 0 ms (100.00%), Space: 6.5 MB (71.44%) - LeetHub

--- 118-pascals-triangle/118-pascals-triangle.cpp
+++ 118-pascals-triangle/118-pascals-triangle.cpp
@@ -1,17 +1,30 @@
 class Solution {
 public:
     vector<vector<int>> generate(int numRows) {
-        if(numRows==1) return {{1}};
-        vector<vector<int>> v=generate(numRows-1);
-        int n=v.size();
-        vector<int> v1;
-        v1.push_back(1);
-        for(int i=1;i<v[v.size()-1].size();i++)
+        // if(numRows==1) return {{1}};
+        // vector<vector<int>> v=generate(numRows-1);
+        // int n=v.size();
+        // vector<int> v1;
+        // v1.push_back(1);
+        // for(int i=1;i<v[v.size()-1].size();i++)
+        // {
+        //     v1.push_back(v[v.size()-1][i-1]+v[v.size()-1][i]);
+        // }
+        // v1.push_back(1);
+        // v.push_back(v1);
+        // return v;
+        vector<vector<int>> ans;
+        for(int i=1;i<=numRows;i++)
         {
-            v1.push_back(v[v.size()-1][i-1]+v[v.size()-1][i]);
+            int c=1;
+            vector<int> v;
+            for(int j=1;j<=i;j++)
+            {
+                v.push_back(c);
+               c=c*(i-j)/j; 
+            }
+            ans.push_back(v);
         }
-        v1.push_back(1);
-        v.push_back(v1);
-        return v;
+        return ans;
     }
 };
