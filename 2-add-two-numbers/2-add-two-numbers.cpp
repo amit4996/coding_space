4df238c8fc1adda02df4fc1995b34b5a2a4a32a8
amit4996 <69569478+amit4996@users.noreply.github.com>
2022-05-27 18:10:50 +0530

    Time: 45 ms (60.28%), Space: 71.4 MB (50.74%) - LeetHub

--- 2-add-two-numbers/2-add-two-numbers.cpp
+++ 2-add-two-numbers/2-add-two-numbers.cpp
@@ -16,22 +16,40 @@ public:
         int sum=0;
         ListNode *head=new ListNode();
         ListNode *curr=head;
-        while(l1!=NULL || l2!=NULL)
+        while(l1!=NULL && l2!=NULL)
         {
-            int x=(l1!=NULL)?l1->val:0;
-            int y=(l2!=NULL)?l2->val:0;
-            temp=x+y+carry;
+            temp=(l1->val+l2->val)+carry;
             sum=temp%10;
             carry=temp/10;
             ListNode *temp=new ListNode(sum);
             curr->next=temp;
             curr=temp;
-            if(l1!=NULL)l1=l1->next;
-            if(l2!=NULL)l2=l2->next;
+            l1=l1->next;
+            l2=l2->next;
+        }
+        while(l1!=NULL)
+        {
+            temp=(l1->val)+carry;
+            sum=temp%10;
+            carry=temp/10;
+            ListNode *temp=new ListNode(sum);
+            curr->next=temp;
+            curr=temp;
+            l1=l1->next;
+        }
+        while(l2!=NULL)
+        {
+            temp=(l2->val)+carry;
+            sum=temp%10;
+            carry=temp/10;
+            ListNode *temp=new ListNode(sum);
+            curr->next=temp;
+            curr=temp;
+            l2=l2->next;
         }
         if(carry)
         {
-            curr->next=new ListNode(carry);
+            curr->next= new ListNode(carry);
         }
         return head->next;
     }
