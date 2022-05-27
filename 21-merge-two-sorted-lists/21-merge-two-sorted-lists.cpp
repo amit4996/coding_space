b60851c0f5453f862bfd0eda9589a71c055c4a7a
amit4996 <69569478+amit4996@users.noreply.github.com>
2022-05-27 10:38:10 +0530

    Time: 3 ms (97.75%), Space: 14.9 MB (14.97%) - LeetHub

---
+++ 21-merge-two-sorted-lists/21-merge-two-sorted-lists.cpp
@@ -0,0 +1,64 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* mergeTwoLists(ListNode* list1, ListNode* list2) {
+       ListNode* l1=list1;
+        ListNode* l2=list2;
+        if(l1==NULL && l2!=NULL) return l2;
+        if(l2==NULL && l1!=NULL) return l1;
+        if(l1==NULL && l2==NULL) return NULL;
+        ListNode* head=new ListNode();
+        ListNode* curr=NULL;
+        if(l1->val<=l2->val)
+        {
+            ListNode* temp=new ListNode(l1->val);
+            head->next=temp;
+            curr=temp;
+            l1=l1->next;
+        }
+        else
+        {
+            ListNode* temp=new ListNode(l2->val);
+            head->next=temp;
+            curr=temp;
+            l2=l2->next;
+        }
+        while(l1!=NULL && l2!=NULL)
+        {
+            if(l1->val<=l2->val)
+            {
+                ListNode* temp=new ListNode(l1->val);
+                curr->next=temp;
+                curr=temp;
+                l1=l1->next;
+            }
+            else
+            {
+                
+                ListNode* temp=new ListNode(l2->val);
+                curr->next=temp;
+                curr=temp;
+                l2=l2->next;
+            }
+        }
+        if(l1!=NULL)
+        {
+            curr->next=l1;
+        }
+        if(l2!=NULL)
+        {
+            curr->next=l2;
+        }
+        return head->next;
+        
+    }
+};
