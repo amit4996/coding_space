96a116b16e1b4a0ab76cb75cfb154f0da0c14cd8
amit4996 <69569478+amit4996@users.noreply.github.com>
2022-05-29 18:41:05 +0530

    Time: 18 ms (13.48%), Space: 15 MB (14.93%) - LeetHub

--- 21-merge-two-sorted-lists/21-merge-two-sorted-lists.cpp
+++ 21-merge-two-sorted-lists/21-merge-two-sorted-lists.cpp
@@ -18,20 +18,6 @@ public:
         if(l1==NULL && l2==NULL) return NULL;
         ListNode* head=new ListNode();
         ListNode* curr=head;
-        // if(l1->val<=l2->val)
-        // {
-        //     ListNode* temp=new ListNode(l1->val);
-        //     head->next=temp;
-        //     curr=temp;
-        //     l1=l1->next;
-        // }
-        // else
-        // {
-        //     ListNode* temp=new ListNode(l2->val);
-        //     head->next=temp;
-        //     curr=temp;
-        //     l2=l2->next;
-        // }
         while(l1!=NULL && l2!=NULL)
         {
             if(l1->val<=l2->val)
