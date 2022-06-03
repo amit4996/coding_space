1697664a2221331b5337bd9a45175a1ea8816288
amit4996 <69569478+amit4996@users.noreply.github.com>
2022-06-03 21:00:07 +0530

    Time: 35 ms (8.13%), Space: 20.9 MB (81.53%) - LeetHub

--- 110-balanced-binary-tree/110-balanced-binary-tree.cpp
+++ 110-balanced-binary-tree/110-balanced-binary-tree.cpp
@@ -11,20 +11,17 @@
  */
 class Solution {
 public:
-    int dfsheight(TreeNode* root)
+    bool flag=true;
+    int height(TreeNode* root)
     {
         if(!root) return 0;
-        
-        int l=dfsheight(root->left);
-        int r=dfsheight(root->right);
-        
-        if(l==-1 || r==-1) return -1;
-        if(abs(l-r)>1) return -1;
-        
-        return 1+max(l,r);
+        int l=height(root->left);
+        int r=height(root->right);
+        if(abs(l-r)>1) flag=false;
+        return max(l,r)+1;
     }
     bool isBalanced(TreeNode* root) {
-        if(dfsheight(root)==-1) return false;
-        return true;
+        height(root);
+        return flag;
     }
 };
