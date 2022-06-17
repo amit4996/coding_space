2cf28834da30ba381557580696e94f3754109ccf
amit4996 <69569478+amit4996@users.noreply.github.com>
2022-06-17 12:02:00 +0530

    Time: 228 ms (26.35%), Space: 61.1 MB (90.98%) - LeetHub

--- 287-find-the-duplicate-number/287-find-the-duplicate-number.cpp
+++ 287-find-the-duplicate-number/287-find-the-duplicate-number.cpp
@@ -6,13 +6,13 @@ public:
         sort(nums.begin(),nums.end());
        for(int i=1;i<nums.size();i++)
        {
-           if((nums[i-1]^nums[i])==0)
+           if((nums[i-1]^nums[i])==0) //if(nums[i-1]==nums[i]) return nums[i];
              
            {
                ans=nums[i-1];
                break;
            }
        }
-        return ans;
+        return ans; //return -1;
     }
 };
